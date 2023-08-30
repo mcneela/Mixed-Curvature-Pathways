d3870790e6d2b8a5ed322a67ffec4c841999a42d
Daniel McNeela <daniel.mcneela@gmail.com>
2023-08-30 13:20:26 -0500

    add graph laplacian embedding

---
+++ multiset/graph_laplacian_embed.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import networkx as nx
+
+from argparser import ArgumentParser
+
+def embed_graph(fpath):
+    G = nx.read_edgelist(fpath)
+    L = nx.laplacian_matrix(G)
+    evals, M = np.linalg.eig(L)
+    return M
+
+def save_embed(outpath, M):
+    np.save(outpath, M)
+
+def embed_all(folder):
+    for graph_num in os.listdir(folder):
+        edge_path = os.path.join(folder, graph_num, f"{graph_num}.edges")
+        M = embed_graph(edge_path)
+        outpath = os.path.join(f"{folder}_laplacian_models", f"{graph_num}.npy")
+        save_embed(outpath, M)
+    
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", type=str, help="name of dataset")
+    args = parser.parse_args()
+    dataset = args.dataset
+    embed_all(dataset)
